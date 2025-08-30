b5a02b8b188f711af54bbf95eba2c110dc621e93
Muba2023 <mubarak161099@gmail.com>
2025-08-31 04:46:20 +0530

    Update Test5Application.java
    midified classes

--- test5/src/main/java/com/test5/Test5Application.java
+++ test5/src/main/java/com/test5/Test5Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test5Application {
 	int x  = 100;
-
+    String s = "mike";
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
 	}
